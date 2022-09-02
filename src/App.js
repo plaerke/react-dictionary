06926536821f41c2280922eb978089a9512fe30e
plaerke <plaerke@me.com>
2022-09-02 17:03:32 +0200

    Created react app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="hello-world">Hello world!</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
