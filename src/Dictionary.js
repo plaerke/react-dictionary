7b5ff6f24d451f10d951c651b5c83268c38aba83
plaerke <plaerke@me.com>
2022-09-06 12:08:25 +0200

    Added default search word

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,8 +4,9 @@ import axios from "axios";
 import Results from "./Results.js";
 
 export default function Dictionary() {
-  const [word, setWord] = useState("");
+  const [word, setWord] = useState("naughty");
   const [wordData, setWordData] = useState(null);
+  const [loaded, setLoaded] = useState(false);
 
   function handleWordChange(event) {
     event.preventDefault();
@@ -14,30 +15,38 @@ export default function Dictionary() {
 
   function handleResponse(response) {
     setWordData(response.data[0]);
+    setLoaded(true);
   }
 
-  function search(event) {
-    event.preventDefault();
-
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
     axios.get(apiUrl).then(handleResponse);
   }
 
-  return (
-    <div className="Dictionary">
-      <h4>What word are you looking for?</h4>
-      <form onSubmit={search}>
-        <div className="word-search-submit">
-          <input
-            type="search"
-            placeholder="Type a word here"
-            onChange={handleWordChange}
-            className="word-search"
-          />
-          <input type="submit" value="Search" className="word-submit" />
-        </div>
-      </form>
-      <Results data={wordData} />
-    </div>
-  );
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+
+  if (loaded) {
+    return (
+      <div className="Dictionary">
+        <h4>What word are you looking for?</h4>
+        <form onSubmit={handleSubmit}>
+          <div className="word-search-submit">
+            <input
+              type="search"
+              placeholder="Type a word here"
+              onChange={handleWordChange}
+              className="word-search"
+            />
+            <input type="submit" value="Search" className="word-submit" />
+          </div>
+        </form>
+        <Results data={wordData} />
+      </div>
+    );
+  } else {
+    return search();
+  }
 }
